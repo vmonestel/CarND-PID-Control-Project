55be47bc55e55baa98335b644dc1d74396abe392
Vianney Monestel Alfaro <vmonestel@hpe.com>
2018-06-29 12:55:22 -0600

    Add main.cpp modifications
    * Add coefficientes for the proportional, integral and derivative
    errors. These coeffs were tuned manually.
    * If the integral coefficient is greater than cero (either a big or
    small value) the car will drive in circles and it goes out the
    track very fast; so it should be zero
    * If the proportional coeff is around 1.0, the car can go easier
    out of the track, so 0.15 is a good value for that.
    * When the derivative coeff is around 3 or 4; it produces large
    steering angles and the driving turn really rough. By reducing it,
    the car drives in a smoother and more human mode.
    * Calculate the steer value depending on the CTE value
    * Check for valid values of steering [-1, 1]

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.15, 0.0, 0.9);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -48,16 +49,19 @@ int main()
         if (event == "telemetry") {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          
+          double steer_value = 0.0;
+
+          // Calculate the steering value
+          pid.UpdateError(cte);
+          steer_value += pid.TotalError();
+
+          // Check for invalid steering values
+          if(steer_value > 1.0) {
+              steer_value = 1.0;
+          } else if (steer_value < -1.0) {
+              steer_value = -1.0;
+          }
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
